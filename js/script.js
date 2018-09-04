3e54a8c88ebebaf0328eb874f9b765dc14d7ac48
Joanna Buchała <joanna.b2@op.pl>
2018-09-04 22:14:35 +0200

    add some styles, function to fade in text

--- js/script.js
+++ js/script.js
@@ -1,32 +1,53 @@
-$('#about_scroll').on('click', function() {
-	$('html, body').animate({
-		scrollTop: $('#about').offset().top },
-		1000
-	);
-});
+$(function() {
 
-$('#knowledge_scroll').on('click', function() {
-	$('html, body').animate({
-		scrollTop: $('#knowledge').position().top },
-		1000
-	);
-});
+	$('div.hello').fadeIn(1500).removeClass('hello');
 
-$('#in_touch_scroll').on('click', function() {
-	$('html, body').animate({
-		scrollTop: $('#in_touch').position().top },
-		1000
-	);
-});
+	$(window).on('scroll', function() {
+		$('.hide').each( function(i) {
+			var bottom_obj = $(this).offset().top + $(this).outerHeight();
+			var bottom_win = $(window).scrollTop() + $(window).height();
 
-$('#up').on('click', function() {
-	$('html, body').animate({
-		scrollTop: $('#hello').position().top },
-		1000
-	);
-});
+			if(bottom_win > bottom_obj) {
+				$(this).animate({'opacity':'1'}, 700);
+			}
+		});
+	});
 
-$(window).on('scroll', function() {
-	if($(this).scrollTop() > 300) $('#up').fadeIn();
-	else $('#up').fadeOut();
+	$('#about_scroll').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('#about').position().top },
+			1000
+		);
+	});
+
+	$('#knowledge_scroll').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('#knowledge').position().top },
+			1000
+		);
+	});
+
+	$('#in_touch_scroll').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('#in_touch').position().top },
+			1000
+		);
+	});
+
+	$('#up').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('#welcome').position().top },
+			1000
+		);
+	});
+
+	$(window).on('scroll', function() {
+		if($(this).scrollTop() > 300) $('#up').fadeIn();
+		else $('#up').fadeOut();
+	});
+
+	$('a').on('dragstart', function() {
+		return false;
+	});
+
 });
